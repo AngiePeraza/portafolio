0115212968c1cba53fd979df140031ef914ec3c9
Angie Peraza <lorep19@gmail.com>
2018-02-13 20:41:49 -0500

    Rutas creadas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
+//components
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { PortafolioComponent } from './components/portafolio/portafolio.component';
 import { AboutComponent } from './components/about/about.component';
 import { PortafolioItemComponent } from './components/portafolio-item/portafolio-item.component';
+
+//routes
+import { APP_ROUTING } from './app.routes';
 
 
 @NgModule({
@@ -20,7 +23,8 @@ import { PortafolioItemComponent } from './components/portafolio-item/portafolio
     PortafolioItemComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    APP_ROUTING
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/app.routes.ts
@@ -0,0 +1,11 @@
+import { RouterModule, Routes }  from "@angular/router";
+import { AboutComponent, PortafolioComponent, PortafolioItemComponent } from "./components/index.paginas";
+
+const APP_ROUTES: Routes = [
+    { path: '', component: PortafolioComponent },
+    { path: 'about', component: AboutComponent },
+    { path: 'item', component: PortafolioItemComponent },
+    { path: '**', pathMatch: 'full', redirectTo: '' }
+];
+
+export const APP_ROUTING = RouterModule.forRoot(APP_ROUTES, { useHash: true });
