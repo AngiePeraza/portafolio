fec33221bea9f936bec42f6cec03d20789687081
Angie Peraza <lorep19@gmail.com>
2018-02-13 21:03:28 -0500

    Modificaciones al menu

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,10 @@ import { RouterModule, Routes }  from "@angular/router";
 import { AboutComponent, PortafolioComponent, PortafolioItemComponent } from "./components/index.paginas";
 
 const APP_ROUTES: Routes = [
-    { path: '', component: PortafolioComponent },
+    { path: 'home', component: PortafolioComponent },
     { path: 'about', component: AboutComponent },
     { path: 'item', component: PortafolioItemComponent },
-    { path: '**', pathMatch: 'full', redirectTo: '' }
+    { path: '**', pathMatch: 'full', redirectTo: 'home' }
 ];
 
 export const APP_ROUTING = RouterModule.forRoot(APP_ROUTES, { useHash: true });
